96898e99b7803fa459da44d5d9a873a80bde0f88
SushilMallRC <sushil.mall@ringcentral.com>
2024-04-12 18:01:04 +0530

    Added jsdoc

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import { greet } from "./sample";
-
+/**
+ * Calling Greet method for printing hello word.
+ */
 const message: string = greet("World");
 console.log(message);
--- src/sample.ts
+++ src/sample.ts
@@ -1,3 +1,8 @@
+/**
+ * This function retuirn Hello ..
+ * @param name
+ * @returns Hello and Name which passed
+ */
 export function greet(name: string): string {
   return `Hello, ${name}!`;
 }
