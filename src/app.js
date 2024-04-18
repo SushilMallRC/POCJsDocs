f786d620d24744d37c7d3cb9d993186c762b3b1d
SushilMallRC <sushil.mall@ringcentral.com>
2024-04-18 16:31:42 +0530

    Typo

--- src/app.js
+++ src/app.js
@@ -5,11 +5,21 @@ import { greet, test } from "./sample";
 let message = greet("World");
 console.log(message);
 
+/**
+ * Calling Test method for printing Test word.
+ */
 let m = test("Test");
 console.log(m);
+
+/**
+ * Calling Test1 method for printing hello word.
+ */
 
 let m1 = test1("Test1");
 console.log(m1);
 
+/**
+ * Calling Test2 method for printing hello word.
+ */
 let m2 = test("Test2");
 console.log(m2);
