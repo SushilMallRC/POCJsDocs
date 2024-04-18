828bc3a393a5282945877042fdd8dbc61122e815
SushilMallRC <sushil.mall@ringcentral.com>
2024-04-18 16:50:29 +0530

    WIP

---
+++ dist/app.js
@@ -0,0 +1,31 @@
+import { greet, test, test2, test4 } from "./sample";
+/**
+ * Calling Greet method for printing hello word.
+ */
+let message = greet("World");
+console.log(message);
+
+/**
+ * Calling Test method for printing Test word.
+ */
+let m = test("Test");
+console.log(m);
+
+/**
+ * Calling Test1 method for printing hello word.
+ */
+
+let m1 = test2("Test1");
+console.log(m1);
+
+/**
+ * Calling Test2 method for printing hello word.
+ */
+let m2 = test3("Test2");
+console.log(m2);
+
+/**
+ * Calling Test4 method for printing hello word.
+ */
+let m4 = test4("Test2");
+console.log(m4);
---
+++ dist/sample.ts
@@ -0,0 +1,46 @@
+/**
+ * This function retuirn Hello ..
+ * @param name
+ * @returns Hello and Name which passed
+ */
+export function greet(name: string): string {
+  return `Hello, ${name}!`;
+}
+
+/**
+ * Test Method is being called
+ * @param name
+ * @returns
+ */
+export function test(name: string): string {
+  return `Hello, ${name}!`;
+}
+
+/**
+ * Test2 method is being called
+ * @param name
+ * @returns
+ */
+export function test2(name: string): string {
+  return `Hello, ${name}!`;
+}
+
+/**
+ * Test3 method is being called
+ * @param name
+ * @returns
+ */
+
+export function test3(name: string): string {
+  return `Hello, ${name}!`;
+}
+
+/**
+ * Test4 method is being called
+ * @param name
+ * @returns
+ */
+
+export function test4(name: string): string {
+  return `Hello, ${name}!`;
+}
